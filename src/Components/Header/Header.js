347ca885eaf61e8f9f24006fa8893d53e4f2f576
sampad-sarker <sampadsarker777@gmail.com>
2020-03-22 03:10:16 +0600

    little work

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from '../../image/logo2.png';
+import bgImg from '../../image/bannerbackground.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            {/* <h1>This header</h1> */}
+            <div className="container-fluid">
+                
+                    {/* <a class="navbar-brand" href="#">
+                        <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="logo"/>
+                    </a> */}
+
+                    <div className="d-flex bd-highlight">
+                        <div className="p-2  bd-highlight"><a className="navbar-brand" href="/">
+                        <img src={logo} width="15%" height="5%" alt="logo"/>
+                        </a></div>
+                        <div className="p-2 flex-shrink-1 bd-highlight"><button type="button" className="btn btn-sm"><FontAwesomeIcon icon={faShoppingCart} /></button></div>
+                        <div className="p-2 flex-shrink-1 bd-highlight"><button  type="button" className="btn btn-light">Login</button><button type="button" className="btn btn-success">SignUp</button></div>
+                        {/* <div class="p-2 flex-shrink-1 bd-highlight"><button>Signup</button></div> */}
+                    </div>
+
+                    <div className="row">
+                        <div className="col-12 img-container">
+                            <img src={bgImg} alt="" width="100%"/>
+                            <div className="text-over-img ">
+                               <h1>Best Food Waiting for You</h1>
+                               <nav className="navbar navbar-light bg-light" >
+                                    <form className="form-inline">
+                                        <input className="form-control" width="100%" type="search" placeholder="Search" ></input>
+                                        <button className="btn btn-outline-success " type="submit">Search</button>
+                                    </form>
+                                </nav>
+
+                            </div>
+                        </div>
+                    </div>
+                
+                
+                
+            </div>
+            
+            
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/fakeData/foodItem.js
@@ -0,0 +1,256 @@
+var foodItem=
+[
+  {
+    "id": 1,
+    "title":"Qshgh Dfhfh Ffgh",
+    "shortDescription":"loer sdgdfg gfsdg fdggdsg ",
+    "price":12.99,
+    "detailDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem qui facilis maiores et ipsa, praesentium ipsam tempore reiciendis quaerat dolorum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quisquam velit labore inventore deserunt odit asperiores impedit alias, quos fugiat, eos, officia debitis maxime dicta? Alias asperiores culpa tempora corporis. ggsgsdd",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/Gkcz6hp/lunch6.png",
+    
+    
+    
+  },
+
+  {
+    "id": 2,
+    "title":"KKshgh",
+    "shortDescription":"dgdsdg dgsfgsdfg gdfs gsfg",
+    "price":11.99,
+    "detailDescription":"Lorem Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia pariatur delectus officia fugit magni aliquam, aperiam fuga, nihil sunt suscipit iusto praesentium sequi quidem ipsam enim molestias quibusdam beatae quam! gdfgdfgfdgfg loeeeee dfhfghdfhdfh",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+  
+  {
+    "id": 3,
+    "title":"DFshgh",
+    "shortDescription":"dgdsdg dgsfgsdfg gdfs gsfg",
+    "price":9.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda itaque magni architecto ipsum, cupiditate quasi soluta nam asperiores neque rem veritatis earum libero. Sapiente nam sed quos dolorem aliquam ducimus? loerm ggsgsdd",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+  {
+    "id": 4,
+    "title":"WShgh",
+    "shortDescription":"dgdsdg dgsfgsdfg gdfs gsfg",
+    "price":7.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolores vitae beatae aliquid nesciunt, odio dolorem dolore porro voluptates deserunt architecto magni laboriosam. Ut animi odit nemo aut reprehenderit quibusdam? loerm ggsgsdd",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 5,
+    "title":"SDhgh",
+    "shortDescription":"dgdsdg dgsfgsdfg gdfs gsfg",
+    "price":5.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto, adipisci ea nulla rerum, consequuntur, aut exercitationem minima accusamus unde repudiandae nihil cum? Itaque accusantium quisquam, quo esse praesentium iusto. loerm ggsgsdd",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 6,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":10.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae labore odit eligendi error eum quo minima dignissimos magni dolores laboriosam obcaecati consequatur, expedita dolor. Quam aperiam enim repellat consectetur expedita. loerm ggsgsdd",
+    "category":"lunch",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 7,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":17.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facilis consequuntur sint debitis sequi repellat iure culpa enim atque distinctio, magni numquam ipsa est adipisci. Omnis temporibus optio a harum. loerm ggsgsdd lo",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 8,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":18.99,
+    "detailDescription":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius obcaecati adipisci unde voluptate aspernatur laborum, quo ducimus vitae ratione, qui, quaerat error distinctio numquam illo! Dolor quaerat dicta aliquid ratione. loerm ggsgsdd",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 9,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":19.99,
+    "detailDescription":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat nihil mollitia animi consequatur quae temporibus maiores, aut dignissimos impedit eius ducimus velit nesciunt? Deleniti, aperiam quisquam sed vitae atque velit? loerm ggsgsdd",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 10,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":120.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio dolore voluptas atque nemo vero, deleniti eius molestiae. Aliquid quod tempore dolore libero culpa iusto est eius harum, possimus praesentium laborum. loerm ggsgsdd",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 11,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":170.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum distinctio ab ullam perferendis aut eius placeat, accusamus doloribus tempore unde recusandae est quod vero neque rem veritatis voluptatum architecto obcaecati.  loerm ggsgsdd",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 12,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":10.99,
+    "detailDescription":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro optio animi sequi dolor! Eum doloremque quod tenetur consectetur fugiat beatae iste quas voluptate ex expedita facilis sit, dolores repudiandae voluptates! loerm ggsgsdd",
+    "category":"dinner",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 13,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":33.99,
+    "detailDescription":"loerm ggsgsdd",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 14,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":9.99,
+    "detailDescription":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo veritatis corporis minus beatae accusamus ipsum? Ratione perspiciatis voluptate magnam mollitia velit atque quis dicta quisquam omnis, non dolorem culpa nihil. loerm ggsgsdd",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 15,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":1.99,
+    "detailDescription":"loerm Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt debitis cupiditate at inventore mollitia temporibus, assumenda sapiente fuga harum sequi voluptates ab quis numquam accusantium voluptatem, in delectus suscipit quaerat? ggsgsdd lo lo",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 16,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":7.99,
+    "detailDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore voluptatibus optio est voluptatum amet, eligendi accusantium modi eaque atque temporibus placeat suscipit porro vero voluptatem omnis facilis exercitationem illo perferendis.  loerm ggsgsdd",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 17,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":7.99,
+    "detailDescription":"loerm Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tempore placeat sed nostrum corporis at ab explicabo velit id deserunt, maiores libero dignissimos tenetur magnam dicta amet commodi autem aliquam! ggsgsdd",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  },
+
+  {
+    "id": 18,
+    "title":"SDhgh DSgdg fgdfg",
+    "shortDescription":"KKdsdg dgsf gsdfgdfs gsfg",
+    "price":8.99,
+    "detailDescription":"loermfds Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus culpa labore veniam ducimus possimus, dicta a ea excepturi quibusdam, voluptatem nesciunt. Ducimus culpa molestias repudiandae porro voluptates adipisci eaque doloremque? ggsgsdd",
+    "category":"breakfast",
+    
+    "img":"https://i.ibb.co/F3SjtfW/jonnelle-yankovich-Rn-Sfyz-Ea-8-E-unsplash.jpg",
+    
+    
+    
+  }
+  
+];
+
+export default foodItem;
